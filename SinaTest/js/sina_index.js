c229b3d63db194d2784556391e4aeb9ab516ab4f
Li Xin <1289885769@qq.com>
2017-08-24 23:19:43 +0800

    update

--- SinaTest/js/sina_index.js
+++ SinaTest/js/sina_index.js
@@ -30,11 +30,11 @@ $(document).ready(function(){
 			index=0;
 		}	
 		changeTo(index);
-	},2500);
+	},3000);
 
 	function changeTo(num){
 		var goLeft=num*240;
-		$('.imglist').animate({left:"-"+goLeft+"px"},1000);	
+		$('.imglist').animate({left:"-"+goLeft+"px"},2000);	
 	}
 });
 
@@ -61,3 +61,40 @@ $(document).ready(function(){
 	var tabCont3=$('.switch-div3>div');
 	switchDiv(orderMenu3,tabCont3);
 })	
+
+
+/*************新闻轮播***********************/
+$(document).ready(function(){
+	var list=$('.news-switch-m ul');
+	var dot=$('.mod-guess-dots span');
+	var index=0;
+	dot.eq(index).find('img').attr('src','img/小图标黄.png').parent().siblings().find('img').attr('src','img/小图标灰.png');
+	dot.hover(function(){
+		index=$(this).index();
+		$(this).find('img').attr('src','img/小图标黄.png').parent().siblings().find('img').attr('src','img/小图标灰.png');
+		list.eq(index).css('display','block').siblings().css('display','none');
+	});
+	var prev=$('.mod-guess-prev');
+	prev.click(function(){		
+		if(index>0){
+			index--;
+		}
+		else{
+			index=list.length-1;
+		}
+		dot.eq(index).find('img').attr('src','img/小图标黄.png').parent().siblings().find('img').attr('src','img/小图标灰.png');
+		list.eq(index).css('display','block').siblings().css('display','none');
+	});
+	var next=$('.mod-guess-next');
+	next.click(function(){		
+		if(index<list.length-1){
+			index++;
+		}
+		else{
+			index=0;
+		}
+		dot.eq(index).find('img').attr('src','img/小图标黄.png').parent().siblings().find('img').attr('src','img/小图标灰.png');
+		list.eq(index).css('display','block').siblings().css('display','none');
+	});
+	
+});
